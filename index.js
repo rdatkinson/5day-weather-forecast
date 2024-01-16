28ce207a1dbb1a98ff6432cab417e67ec3d383fe
Rachel Atkinson <r.d.atkinson97@gmail.com>
2024-01-16 20:24:14 +0000

    added event listener to search button

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const apiKey = '19489d2909dbb2f5c069fecee4cbb60c';
+
+// Add event listener to the search button
+document.getElementById('search-button').addEventListener('click', function() {
+    const city = document.getElementById('city-input').value;
+    if (city) {
+        getCoordinates(city);
+    }
+});
